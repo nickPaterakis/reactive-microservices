aad9788554996247783cd86f6bfce5da8bc3065e
Nicholas Paterakis <nikolas.paterakis@gmail.com>
2021-05-20 12:08:03 +0300

    Some changes

--- booking-client/src/utils/keycloak.js
+++ booking-client/src/utils/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
-  url: 'http://localhost:8090/auth', 
+  url: 'http://localhost:8080/auth', 
   realm: 'booking', 
   clientId: 'booking-react-app-client',
 };
