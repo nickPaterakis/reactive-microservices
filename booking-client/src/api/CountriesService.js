66e56359f49274d3a11e2aed5f0e6b7ac74ddcd2
Nicholas Paterakis <nikolas.paterakis@gmail.com>
2021-08-07 11:03:39 +0300

    Some changes

--- booking-client/src/api/CountriesService.js
+++ booking-client/src/api/CountriesService.js
@@ -4,7 +4,3 @@ import { config } from '../constants/systemConstants';
 export function getCountries(name) {
   return axios.get(`${config.url.API_PROPERTY_URL}/countries/${name}`); 
 }
-
-const receiveAllCountries = () => axios.get('http://35.193.38.160:8765/countries');
-
-export default receiveAllCountries;
